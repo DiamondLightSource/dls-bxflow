0be5dd3bcb6571e51a6ae7e3ce8deb2f93598e33
David Erb <david.erb@diamond.ac.uk>
2023-06-08 13:05:53 +0100

    adds sbatcher and first test of it

--- src/dls_bxflow_lib/bx_launchers/sbatcher.py
+++ src/dls_bxflow_lib/bx_launchers/sbatcher.py
@@ -3,6 +3,7 @@ import json
 import logging
 import os
 import subprocess
+from typing import Dict
 
 # Utilities.
 from dls_utilpack.callsign import callsign
@@ -31,13 +32,66 @@ thing_type = ClassTypes.SBATCHER
 class SlurmerLaunchInfo(BaseLaunchInfo):
     """Launch info specific to this launcher type needed to identify a launched task."""
 
+    FINISHED_JOB_STATES = [
+        "COMPLETED",  # The job has successfully completed execution.
+        "CANCELLED",  # The job was canceled by the user or the system administrator before completion.
+        "FAILED",  # The job encountered an error during execution and did not complete successfully.
+        "TIMEOUT",  # The job exceeded its time limit and was terminated by the system.
+        "NODE_FAIL",  # One or more nodes allocated to the job have failed, leading to job termination.
+        "OUT_OF_MEMORY",  # The job exceeded the memory limits and was terminated.
+        "PREEMPTED",  # The job was preempted by a higher-priority job or system event.
+    ]
+
+    UNFINISHED_JOB_STATES = [
+        "PENDING",  # The job is waiting to be scheduled and has not started running yet.
+        "RUNNING",  # The job is currently running on a compute node.
+        "SUSPENDED",  # The job has been suspended and is temporarily halted. This can happen if the job exceeds resource limits or due to user intervention.
+        "COMPLETING",  # The job has finished running, but some post-processing or cleanup tasks are still in progress.
+    ]
+
     def __init__(self, bx_job, bx_task):
         BaseLaunchInfo.__init__(self, bx_job, bx_task)
-        self.job_number = None
+        self.job_id = None
+        self.job_state = None
 
     def serialize(self):
         """Serialize the launch info for storing in a persistent database."""
-        return json.dumps({"job_number": self.job_number})
+        return json.dumps(
+            {
+                "job_id": self.job_id,
+                "job_state": self.job_state,
+            }
+        )
+
+    def is_finished(
+        self,
+        squeue_jobs_dict: Dict,
+    ) -> bool:
+        """
+        Return true job is finished.
+
+        Args:
+            squeue_jobs_dict (Dict): the squeue jobs list, indexed by job_id
+
+        Returns:
+            bool: True if the job's entry in the squeue response indicates the job is finished
+        """
+        job = squeue_jobs_dict.get(self.job_id)
+
+        # Job not listed in the squeue data?
+        if job is None:
+            logger.debug(f"[SQUJOB] {self.job_id} done because not in dict")
+            # Presume it has finished.
+            return True
+
+        self.job_state = job["job_state"]
+        # Job's state indicates not finished?
+        if self.job_state not in self.UNFINISHED_JOB_STATES:
+            logger.debug(f"[SQUJOB] {self.job_id} done because state {self.job_state}")
+            return True
+
+        # Else presume the job is still running.
+        return False
 
 
 # ------------------------------------------------------------------------------------------
@@ -111,7 +165,7 @@ class Slurmer(BxLauncherBase):
         stderr_filename = "%s/stderr.txt" % (runtime_directory)
 
         command = []
-        command.extend(["qsub"])
+        command.extend(["sbatch"])
         command.extend(["-N", job_name])
         if self.__cluster_project is not None:
             command.extend(["-P", self.__cluster_project])
@@ -123,9 +177,9 @@ class Slurmer(BxLauncherBase):
         if remex_hints is None:
             remex_hints = {}
 
-        # Options for qsub based on the remex hints.
-        qsub_options = {}
-        qsub_l_options = {}
+        # Options for sbatch based on the remex hints.
+        sbatch_options = {}
+        sbatch_l_options = {}
 
         # The following keywords are honored:
         # cluster
@@ -144,7 +198,7 @@ class Slurmer(BxLauncherBase):
         # -------------------------------------------------------------------------------
         t = remex_hints.get(RemexKeywords.REDHAT_RELEASE)
         if t is not None:
-            qsub_l_options["redhat_release"] = t
+            sbatch_l_options["redhat_release"] = t
 
         # -------------------------------------------------------------------------------
         memory_limit = remex_hints.get(RemexKeywords.MEMORY_LIMIT)
@@ -160,18 +214,18 @@ class Slurmer(BxLauncherBase):
                     f' remex_hints[{RemexKeywords.MEMORY_LIMIT}] "{memory_limit}"'
                 )
 
-            qsub_l_options["m_mem_free"] = f"{gigabytes}G"
+            sbatch_l_options["m_mem_free"] = f"{gigabytes}G"
 
         # -------------------------------------------------------------------------------
         t = remex_hints.get(RemexKeywords.TIME_LIMIT)
         if t is not None:
 
-            qsub_l_options["h_rt"] = t
+            sbatch_l_options["h_rt"] = t
 
         # -------------------------------------------------------------------------------
         t = remex_hints.get(RemexKeywords.QUEUE)
         if t is not None:
-            qsub_options["-q"] = t
+            sbatch_options["-q"] = t
 
         # -------------------------------------------------------------------------------
         t = remex_hints.get(RemexKeywords.PARALLEL_ENVIRONMENT)
@@ -181,39 +235,39 @@ class Slurmer(BxLauncherBase):
                 raise RuntimeError(
                     f"{RemexKeywords.PARALLEL_ENVIRONMENT} should be of two parts separated by a space"
                 )
-            qsub_options["-pe"] = t[0].strip()
-            qsub_options[">-pe"] = t[1].strip()
+            sbatch_options["-pe"] = t[0].strip()
+            sbatch_options[">-pe"] = t[1].strip()
 
         # -------------------------------------------------------------------------------
         t = remex_hints.get(RemexKeywords.GPU)
         if t is not None:
-            qsub_l_options["gpu"] = t
+            sbatch_l_options["gpu"] = t
 
         # -------------------------------------------------------------------------------
         t = remex_hints.get(RemexKeywords.GPU_ARCH)
         if t is not None:
-            qsub_l_options["gpu_arch"] = t
+            sbatch_l_options["gpu_arch"] = t
 
         # -------------------------------------------------------------------------------
         t = remex_hints.get(RemexKeywords.HOST)
         if t is not None:
-            qsub_l_options["h"] = t
+            sbatch_l_options["h"] = t
 
         # -------------------------------------------------------------------------------
-        # Add the qsub_options to the command line.
-        for qsub_option, qsub_value in qsub_options.items():
-            if not qsub_option.startswith(">"):
-                command.append(qsub_option)
-            if qsub_value is not None and qsub_value != "":
-                command.append(qsub_value)
+        # Add the sbatch_options to the command line.
+        for sbatch_option, sbatch_value in sbatch_options.items():
+            if not sbatch_option.startswith(">"):
+                command.append(sbatch_option)
+            if sbatch_value is not None and sbatch_value != "":
+                command.append(sbatch_value)
 
-        # Add the qsub "-l" and its sub-options to the command line.
-        if len(qsub_l_options) > 0:
-            qsub_l_values = []
-            for qsub_l_option, qsub_l_value in qsub_l_options.items():
-                qsub_l_values.append(f"{qsub_l_option}={qsub_l_value}")
+        # Add the sbatch "-l" and its sub-options to the command line.
+        if len(sbatch_l_options) > 0:
+            sbatch_l_values = []
+            for sbatch_l_option, sbatch_l_value in sbatch_l_options.items():
+                sbatch_l_values.append(f"{sbatch_l_option}={sbatch_l_value}")
             command.append("-l")
-            command.append(",".join(qsub_l_values))
+            command.append(",".join(sbatch_l_values))
 
         # -------------------------------------------------------------------------------
         command.extend(["-o", stdout_filename])
@@ -223,38 +277,38 @@ class Slurmer(BxLauncherBase):
 
         # command_string = bash_filename
 
-        qsubout_filename = "%s/.bxflow/qsubout.txt" % (runtime_directory)
-        qsuberr_filename = "%s/.bxflow/qsuberr.txt" % (runtime_directory)
+        sbatchout_filename = "%s/.bxflow/sbatchout.txt" % (runtime_directory)
+        sbatcherr_filename = "%s/.bxflow/sbatcherr.txt" % (runtime_directory)
 
         # Split the command into arguments/values for readability in the debug.
         readable = (" ".join(command)).replace(" -", " \\\n    -")
         logger.debug(f"{callsign(self)} running command\n{readable}")
 
         while True:
-            with open(qsubout_filename, "wt") as qsubout_handle:
-                with open(qsuberr_filename, "wt") as qsuberr_handle:
+            with open(sbatchout_filename, "wt") as sbatchout_handle:
+                with open(sbatcherr_filename, "wt") as sbatcherr_handle:
                     try:
-                        # Wait until qsub command completes.
-                        # TODO: In slurmer, use asyncio to run qsub.
+                        # Wait until sbatch command completes.
+                        # TODO: In slurmer, use asyncio to run sbatch.
                         completed = subprocess.run(
                             command,
                             shell=False,
                             # input=command_string,
                             text=True,
                             cwd=runtime_directory,
-                            stdout=qsubout_handle,
-                            stderr=qsuberr_handle,
+                            stdout=sbatchout_handle,
+                            stderr=sbatcherr_handle,
                         )
                     except Exception:
-                        raise RuntimeError("failed to execute the qsub command")
+                        raise RuntimeError("failed to execute the sbatch command")
 
-            # The qsub command ran ok.
+            # The sbatch command ran ok.
             if completed.returncode == 0:
                 break
 
-            # The qsub command ran, but it indicates there was a problem.
+            # The sbatch command ran, but it indicates there was a problem.
             lines = []
-            with open(qsuberr_filename, "r") as stream:
+            with open(sbatcherr_filename, "r") as stream:
                 for line in stream:
                     line = line.strip()
                     if line == "":
@@ -264,10 +318,10 @@ class Slurmer(BxLauncherBase):
                     lines.append(line)
 
             if len(lines) == 0:
-                lines.append(f"for cause, see {qsuberr_filename}")
+                lines.append(f"for cause, see {sbatcherr_filename}")
 
             # lines.append(f"executing command: {command_string}")
-            # lines.append("piped into qsub: %s" % (" ".join(command)))
+            # lines.append("piped into sbatch: %s" % (" ".join(command)))
 
             logger.warning(callsign(self, "\n    ".join(lines)))
 
@@ -277,16 +331,16 @@ class Slurmer(BxLauncherBase):
             # TODO: In slurmer launcher, have a limit to number of retries.
             # raise RemoteSubmitFailed("; ".join(lines))
 
-        with open(qsubout_filename, "r") as stream:
-            job_number = stream.read().strip()
+        with open(sbatchout_filename, "r") as stream:
+            job_id = stream.read().strip()
 
         logger.debug(
-            f"{callsign(self)} submitted job_number {job_number} for task {bx_task_uuid}"
+            f"{callsign(self)} submitted job_id {job_id} for task {bx_task_uuid}"
         )
 
         # Make a serializable object representing the entity which was launched.
         launch_info = SlurmerLaunchInfo(bx_job, bx_task)
-        launch_info.job_number = job_number
+        launch_info.job_id = job_id
 
         # Let the base class update the objects in the database.
         await self.post_submit(launch_info)
@@ -300,8 +354,8 @@ class Slurmer(BxLauncherBase):
         launch_info = SlurmerLaunchInfo(bx_job, bx_task)
 
         # Add the fields which were in the serialized string.
-        launch_info.job_number = require(
-            "SlurmerLaunchInfo unserialized", unserialized, "job_number"
+        launch_info.job_id = require(
+            "SlurmerLaunchInfo unserialized", unserialized, "job_id"
         )
 
         return launch_info
@@ -310,64 +364,84 @@ class Slurmer(BxLauncherBase):
     async def are_done(self, launch_infos):
         """Check for done jobs among the list provided."""
 
-        qstat = []
-        qstat.append("qstat")
-        launch_info_job_numbers = []
-        for launch_info in launch_infos:
-            launch_info_job_numbers.append(launch_info.job_number)
-
-        qstat.extend(["-j", ",".join(launch_info_job_numbers)])
+        squeue_command = []
+        squeue_command.append("squeue")
+        squeue_command.append("--json")
 
         try:
             # Wait until shell script completes.
             completed = subprocess.run(
-                qstat,
+                squeue_command,
                 shell=False,
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
             )
         except Exception:
-            raise RuntimeError("failed to execute the qstat command")
-
-        # You get an completioncode of 1 if there are no results.
-        if completed.returncode not in [0, 1]:
-            logger.debug(
-                "qstat got unexpected returncode %s, stderr was:\n%s"
-                % (completed.returncode, completed.stderr.decode())
+            raise RuntimeError(
+                "failed to execute the squeue command '%s'" % (" ".join(squeue_command))
             )
-            raise RuntimeError("qstat failed")
 
-        # logger.debug("qstat stdout was:\n%s" % (completed.stdout.decode()))
+        # Check the return code from squeue.
+        if completed.returncode != 0:
+            logger.debug(
+                "squeue command '%s' got unexpected returncode %s, stderr was:\n%s"
+                % (
+                    " ".join(squeue_command),
+                    completed.returncode,
+                    completed.stderr.decode(),
+                )
+            )
+            raise RuntimeError("squeue failed")
 
-        lines = completed.stdout.decode().split("\n")
+        # logger.debug("squeue stdout was:\n%s" % (completed.stdout.decode()))
 
-        qstat_job_numbers = []
-        for line in lines:
-            if not line.startswith("job_number"):
-                continue
-            parts = line.split(" ", 1)
-            qstat_job_numbers.append(parts[1].strip())
+        # Parse the squeue json report.
+        stdout = completed.stdout.decode()
+        try:
+            squeue_dict = json.loads(stdout)
+        except Exception:
+            logger.debug(f"squeue response:\n{stdout}")
+            raise RuntimeError("squeue response cannot be parsed as json (see log)")
+
+        errors = squeue_dict.get("errors", [])
+        if len(errors) > 0:
+            raise RuntimeError(
+                f"squeue response errors field is not empty\n{str(errors)}"
+            )
+
+        if "jobs" not in squeue_dict:
+            logger.debug(f"squeue response:\n{stdout}")
+            raise RuntimeError("squeue response does not contain jobs field (see log)")
+
+        squeue_jobs_dict = {}
+        for job in squeue_dict["jobs"]:
+            squeue_jobs_dict[job["job_id"]] = job
 
         done_infos = []
         remaining_infos = []
+
+        # Look through all the job infos which we are monitoring.
         for launch_info in launch_infos:
-            if launch_info.job_number not in qstat_job_numbers:
+            # This job looks done according to the squeue output?
+            if launch_info.is_finished(squeue_jobs_dict):
                 done_infos.append(launch_info)
             else:
                 remaining_infos.append(launch_info)
 
-        for done_info in done_infos:
+        for info in done_infos:
             logger.debug(
-                f"[LAUNDON1] cluster job {done_info.job_number}"
-                f" seems done for bx_job {done_info.bx_job.uuid()}"
-                f" bx_task {done_info.bx_task.uuid()}"
+                f"[LAUNDON1] cluster job {info.job_id}"
+                f" state {info.job_state}"
+                f" seems done for bx_job {info.bx_job.uuid()}"
+                f" bx_task {info.bx_task.uuid()}"
             )
 
-        for remaining_info in remaining_infos:
+        for info in remaining_infos:
             logger.debug(
-                f"[LAUNDON2] cluster job {remaining_info.job_number}"
-                f" still remaining for bx_job {remaining_info.bx_job.uuid()}"
-                f" bx_task {remaining_info.bx_task.uuid()}"
+                f"[LAUNDON2] cluster job {info.job_id}"
+                f" state {info.job_state}"
+                f" still remaining for bx_job {info.bx_job.uuid()}"
+                f" bx_task {info.bx_task.uuid()}"
             )
 
         return done_infos, remaining_infos
